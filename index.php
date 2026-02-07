be94899ac83eb52b4496ada13e6ac62779eca516
nexty22 <fakeintro688@gmail.com>
2026-02-07 13:08:25 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>NEXTY • YouTube Downloader</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="box">
+  <h1>🎬 NEXTY Downloader</h1>
+  <p class="tagline">Fast • Clean • Free</p>
+
+  <form action="fetch.php" method="GET">
+    <input type="text" name="url" placeholder="Paste YouTube link here" required>
+    <button type="submit">Fetch Qualities</button>
+  </form>
+
+  <p class="credit">Made by <b>NEXTY</b> ❤️</p>
+</div>
+
+</body>
+</html>
